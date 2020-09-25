2fdbb6d4b33b87ec7757671f7687598b89e148df
ボクはお金で動くよ <70741260+black-triple-star@users.noreply.github.com>
2020-09-25 15:55:35 +0900

    _

--- vim_setup.cmd
+++
@@ -1,4 +0,0 @@
-if not exist "C:\temp\vim\bkup" ( mkdir "C:\temp\vim\bkup" )
-if not exist "C:\temp\vim\swap" ( mkdir "C:\temp\vim\swap" )
-if not exist "C:\temp\vim\undo" ( mkdir "C:\temp\vim\undo" )
-
